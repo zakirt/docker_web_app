f859d1242bbf11d09a53c7cccc9f27364c694c58
Zakir Tariverdiev <zakir-ny@hotmail.com>
2019-03-07 00:48:31 -0500

    Add index HTML file (#3)

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const redis = require('redis');
 const logger = require('./utils/logger');
 
 // Constants
-const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';
+const PORT = process.env.NODE_PORT;
+const HOST = process.env.NODE_HOST;
 const client = redis.createClient(process.env.REDIS_URL);
 
 const app = express();
+const staticPath = path.join(__dirname, 'public');
+
+app.use(express.static(staticPath));
 app.get('/', (req, res) => {
-    logger.logInfo('Hello world\n');
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT);
